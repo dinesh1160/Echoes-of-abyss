8571dc3a3daaf3851ceb6a9c29fc75d70a06e2ec
dinesh1160 <dinesharunachalam116@gmail.com>
2025-05-18 00:38:55 +0530

    added features

--- Player/player.gd
+++ Player/player.gd
@@ -9,6 +9,11 @@ var drag := 400.0
 @onready var End_menu = get_parent().get_node("CanvasLayer/EndMenu")
 
 @onready var sprite: Sprite2D = $Sub
+var min_zoom: float = 0.35
+var max_zoom: float = 0.85
+var zoom_speed: float = 2.0
+var zoom_lerp_speed: float = 0.5
+var game_over_zoom = Vector2(1.5,1.5)
 
 func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("left", "right", "up", "down")
@@ -32,6 +37,13 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, drag * delta)
 
 	move_and_slide()
+	var current_speed = velocity.length()
+	var normalized_speed = clamp(current_speed / speed, 0.0, 1.0)
+	var target_zoom_value = lerp(max_zoom, min_zoom, normalized_speed)
+	var target_zoom = Vector2(target_zoom_value, target_zoom_value)
+
+	# Interpolate zoom smoothly
+	Camera.zoom = Camera.zoom.lerp(target_zoom, zoom_lerp_speed * delta)
 
 
 func _on_lightarea_body_entered(body: Node2D) -> void:
@@ -57,8 +69,9 @@ func _on_hit_area_body_entered(body: Node2D) -> void:
 		game_over_by_attack()
 		
 func game_over_by_attack() -> void:
+	Camera.zoom = Camera.zoom.lerp(game_over_zoom,1)
 	Camera.apply_shake()
-	await get_tree().create_timer(0.8).timeout
+	await get_tree().create_timer(1).timeout
 	Camera.apply_shake()
 	End_menu.display()
 	get_tree().paused = true
